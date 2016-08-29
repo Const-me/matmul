75a238a7c33c37ff5a0e943ce3bd39c239bba207
Konstantin <soonts@live.com>
2016-08-29 04:45:04 +0200

    Minor (results order)

--- Run/run.cmd
+++ Run/run.cmd
@@ -3,6 +3,6 @@ set MM32=..\Win32\Release\MatMul.exe
 set MM=%MM32%
 
 del result.txt
-for %%a in (1,2,7,3,4,8) do for %%n in (2000,4000) do %MM% -a%%a -n%%n 2>> result.txt
+for %%n in (2000,4000) do for %%a in (1,2,7,3,4,8) do %MM% -a%%a -n%%n 2>> result.txt
 rem for %%a in (0,1,2,7,3,4,8) do for %%n in (2000) do %MM% -a%%a -n%%n 2>> result.txt
 pause
